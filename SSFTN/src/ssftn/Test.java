159efbdadd39553d157d4cf8ff3100655bb5e1ea
Milica Vucinic <vucinic.ra139.2018@uns.ac.rs>
2020-11-14 13:15:16 +0100

    jos uvijek proba

--- SSFTN/src/ssftn/Test.java
+++ SSFTN/src/ssftn/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Pozz SSFTN");
 		System.out.println("test test");
+		System.out.println("Drugi komit");
+		
 	}
 
 }
