76c3656af35b6b7915962ce18937271a96f91318
Milica Vucinic <vucinic.ra139.2018@uns.ac.rs>
2020-12-01 15:32:07 +0100

    #profesor ali nekompletan zasada

---
+++ SSFTN/src/model/Profesor.java
@@ -0,0 +1,102 @@
+package model;
+
+public class Profesor {
+	
+	
+
+	private String prezime;
+	private String ime;
+	private String datumRodjenja;
+	private String adresaStanovanja;
+	private String kontaktTelefon;
+	private String email;
+	private String adresaKancelarije;
+	private String brojLicneKarte;
+	private String titula;
+	private String zvanje;
+	
+	//Spisak predmeta na kojima je profesor neka lista, nesto 
+	
+	
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	
+	
+	public String getDatumRodjenja() {
+		return datumRodjenja;
+	}
+	public void setDatumRodjenja(String datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+	
+	
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+	
+	
+	public String getKontaktTelefon() {
+		return kontaktTelefon;
+	}
+	public void setKontaktTelefon(String kontaktTelefon) {
+		this.kontaktTelefon = kontaktTelefon;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String getAdresaKancelarije() {
+		return adresaKancelarije;
+	}
+	public void setAdresaKancelarije(String adresaKancelarije) {
+		this.adresaKancelarije = adresaKancelarije;
+	}
+	
+	
+	public String getBrojLicneKarte() {
+		return brojLicneKarte;
+	}
+	public void setBrojLicneKarte(String brojLicneKarte) {
+		this.brojLicneKarte = brojLicneKarte;
+	}
+	
+	
+	public String getTitula() {
+		return titula;
+	}
+	public void setTitula(String titula) {
+		this.titula = titula;
+	}
+	
+	
+	public String getZvanje() {
+		return zvanje;
+	}
+	public void setZvanje(String zvanje) {
+		this.zvanje = zvanje;
+	}	
+
+	
+
+}
